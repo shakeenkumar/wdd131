fb50d8cfb87e344e8ea1b3ef7de2925bf7980043
shakeen <shakeenkumar33@gmail.com>
2024-11-10 07:27:13 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Display the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Display the last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
