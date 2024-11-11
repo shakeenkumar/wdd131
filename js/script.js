bc58360d8b7ed628b932c5990871fcec9e6a30ef
shakeen <shakeenkumar33@gmail.com>
2024-11-11 17:16:55 +0530

    zdv

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-document.getElementById("last-modified").textContent = document.lastModified;
+// Display the current year in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear;
+
+// Display the last modified date in the footer
+const lastModified = document.lastModified;
+document.getElementById("last-modified").textContent = lastModified;
