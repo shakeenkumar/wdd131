36ea3c78b12491676020dfd84bc3501f03ae8623
shakeen <shakeenkumar33@gmail.com>
2024-11-29 22:44:37 +0530

    sdfv

--- scripts/place.js
+++ scripts/place.js
@@ -3,8 +3,8 @@ function calculateWindChill(temp, windSpeed) {
     if (temp <= 10 && windSpeed > 4.8) {
         return (
             13.12 +
-            0.6215 * temp -
-            11.37 * Math.pow(windSpeed, 0.16) +
+            0.6215 * temp - 
+            11.37 * Math.pow(windSpeed, 0.16) + 
             0.3965 * temp * Math.pow(windSpeed, 0.16)
         ).toFixed(1);
     } else {
